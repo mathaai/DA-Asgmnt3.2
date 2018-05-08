0b82b81ba59761403b85265f6fb6cced19c8e353
Mathew K <kumpalamthanam@gmail.com>
2018-05-08 08:51:38 +0530

    Add files via upload

---
+++ Assignment 32.R
@@ -0,0 +1,8 @@
+# 1. Define matrix mymat by replicating the sequence 1:5 for 4 times and transforming into a matrix, sum over rows and columns
+
+mymat<-matrix(rep(seq(4), 4), ncol = 4)
+#sum over rows
+apply(mymat, 1, sum)
+#sum over columns
+apply(mymat, 2, sum)
+
